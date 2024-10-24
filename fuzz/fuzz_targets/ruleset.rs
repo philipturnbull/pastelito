39d304f614551a33fc335d042ed4b09b4a7df764
Phil Turnbull <philip.turnbull@gmail.com>
2024-10-23 20:03:57 -0400

    Call spans_to_ranges in fuzzer

--- fuzz/fuzz_targets/ruleset.rs
+++ fuzz/fuzz_targets/ruleset.rs
@@ -3,7 +3,19 @@
 mod helpers;
 use helpers::{filter_panics, RULESET};
 use libfuzzer_sys::{fuzz_target, Corpus};
-use pastelito_core::{parsers::MarkdownParser, Document};
+use pastelito_core::{
+    lines::{spans_to_ranges, LineCharRange},
+    parsers::MarkdownParser,
+    Document,
+};
+
+struct FuzzRange {}
+
+impl LineCharRange for FuzzRange {
+    fn new(_start_line: u32, _start_char: u32, _end_line: u32, _end_char: u32) -> Self {
+        Self {}
+    }
+}
 
 fn do_fuzz(data: &[u8]) -> Corpus {
     if let Ok(markdown) = std::str::from_utf8(data) {
@@ -13,12 +25,15 @@ fn do_fuzz(data: &[u8]) -> Corpus {
             let results = ruleset.apply(&doc);
             let (warnings, measurements) = results.into_iter_both();
 
-            for _warning in warnings {
-                // consume iterator
-            }
-            for _measurement in measurements {
-                // consume iterator
-            }
+            let warnings = spans_to_ranges(markdown, warnings);
+            let _num_warnings = warnings
+                .map(|(_range, _warning): (FuzzRange, _)| {})
+                .count();
+
+            let measurements = spans_to_ranges(markdown, measurements);
+            let _num_measurements = measurements
+                .map(|(_range, _measurement): (FuzzRange, _)| {})
+                .count();
         });
 
         Corpus::Keep
